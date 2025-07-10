8a840f10ab911011b60980953b04ce532a7877e4
강지현 <wlgus4083@gmail.com>
2025-07-10 09:08:18 +0900

    [update] section10 useMeno example upload

--- section10/src/components/List.jsx
+++ section10/src/components/List.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./List.css";
 import ListItem from "./ListItem";
+import { useMemo } from "react";
+import { useEffect } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
@@ -21,9 +23,24 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filterTodos = getFilterTodos();
 
+  // 리렌더링 발생 시, todo 전체갯수, todo = isDone = true 갯수, todo = false 갯수
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("useMeno 최적화 되고있습니다.");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((data) => data.isDone === true).length;
+    const notDoneCount = totalCount - doneCount;
+    return { totalCount, doneCount, notDoneCount };
+  }, [todos]);
+
   return (
     <div className="List">
       <h4>Todo List </h4>
+      <div>
+        <p>Total Count : {totalCount}</p>
+        <p>Done Count : {doneCount}</p>
+        <p>Not Done Count : {notDoneCount}</p>
+      </div>
       <input
         value={search}
         placeholder="검색어를 입력하세요"
