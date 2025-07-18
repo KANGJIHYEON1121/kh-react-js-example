03bd7cdc1eccc3a7fbd71537300b2e4a8a7ef49e
강지현 <wlgus4083@gmail.com>
2025-07-18 16:07:40 +0900

    [update] ReadPage 수정, 목록 버튼 추가

--- mall/src/page/todo/ReadPage.jsx
+++ mall/src/page/todo/ReadPage.jsx
@@ -1,16 +1,22 @@
 import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Header from "../../include/Header";
+import useCustomMove from "../../hooks/useCustomMove";
 
 import ReadComponent from "../../components/todo/ReadComponent";
 
 const ReadPage = () => {
   const { tno } = useParams();
+  const { moveToList, moveToModify } = useCustomMove();
 
   return (
     <Container>
       <Header />
-      <ReadComponent tno={tno} />
+      <ReadComponent
+        tno={tno}
+        moveToList={moveToList}
+        moveToModify={moveToModify}
+      />
     </Container>
   );
 };
